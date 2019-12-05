af3f97c28969f52d9f48671feebe102a0c137f81
Manthan Agrawal <agrawalmanthan0@gmail.com>
2019-12-05 17:57:09 +0530

    Read updated

--- 1. Read,Write and show images.py
+++ 1. Read,Write and show images.py
@@ -1,14 +1,14 @@
 import cv2
 
 # read image
-img = cv2.imread("data\\lena.jpg", 0)
+img = cv2.imread("data\\lena.jpg", 1)
 print(img)
 
 # show image
 cv2.imshow("Window name",img)
 
 # keep window running
-cv2.waitKey(5000)
+cv2.waitKey(5000)# 0 -> do not close
 
 # to destroy all running windows
 cv2.destroyAllWindows()
