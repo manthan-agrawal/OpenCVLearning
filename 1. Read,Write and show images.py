c98316532a8127df58b596a9ed457a0105d31113
Manthan Agrawal <agrawalmanthan0@gmail.com>
2020-04-26 00:39:31 +0530

    modified read and write for images

--- 1. Read,Write and show images.py
+++ 1. Read,Write and show images.py
@@ -5,11 +5,17 @@ img = cv2.imread("data\\lena.jpg", 1)
 print(img)
 
 # show image
-cv2.imshow("Window name",img)
+cv2.imshow("Window name", img)
 
-# keep window running
-cv2.waitKey(5000)# 0 -> do not close
+# keep window running + can be used to get which key is typed
+key = cv2.waitKey(5000)   # 0 -> do not close
+print(key)     # Ascii value will be returned
+
 
 # to destroy all running windows
 cv2.destroyAllWindows()
+
+# to write a file
+cv2.imwrite("data\\newFileName.png", img)
+
 
